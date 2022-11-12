177688e647e462dbf3c5ee80af6a347492c3dbf4
0xHegemon <0xhegemon@gmail.com>
2022-11-12 16:25:06 -0500

    build groupsig example with extra scripts

--- scripts/groupsig/build_groupsig.sh
+++ scripts/groupsig/build_groupsig.sh
@@ -24,7 +24,7 @@ echo "DONE ($((end-start))s)"
 
 echo "****GENERATING WITNESS FOR SAMPLE INPUT****"
 start=`date +%s`
-node "$BUILD_DIR"/"$CIRCUIT_NAME"_js/generate_witness.js "$BUILD_DIR"/"$CIRCUIT_NAME"_js/"$CIRCUIT_NAME".wasm input_groupsig.json "$BUILD_DIR"/witness.wtns
+node "$BUILD_DIR"/"$CIRCUIT_NAME"_js/generate_witness.js "$BUILD_DIR"/"$CIRCUIT_NAME"_js/"$CIRCUIT_NAME".wasm input_groupsig1.json "$BUILD_DIR"/witness.wtns
 end=`date +%s`
 echo "DONE ($((end-start))s)"
 
