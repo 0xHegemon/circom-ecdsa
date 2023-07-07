3c6ffb8e5a49b5361e24575c18cf19b12d986779
0xHegemon <0xhegemon@gmail.com>
2023-07-07 17:14:15 -0500

    add n-of-4 groupsig private key

--- scripts/groupsig4/test_groupsig4.sh
+++ scripts/groupsig4/test_groupsig4.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 PHASE1=../../circuits/pot20_final.ptau
-BUILD_DIR=../../build/groupsig
-CIRCUIT_NAME=groupsig
+BUILD_DIR=../../build/groupsig4
+CIRCUIT_NAME=groupsig4
 
 echo "****GENERATING WITNESS FOR SAMPLE INPUT****"
 start=`date +%s`
-node "$BUILD_DIR"/"$CIRCUIT_NAME"_js/generate_witness.js "$BUILD_DIR"/"$CIRCUIT_NAME"_js/"$CIRCUIT_NAME".wasm input_groupsig_3.json "$BUILD_DIR"/witness.wtns
+node "$BUILD_DIR"/"$CIRCUIT_NAME"_js/generate_witness.js "$BUILD_DIR"/"$CIRCUIT_NAME"_js/"$CIRCUIT_NAME".wasm input_groupsig4_1.json "$BUILD_DIR"/witness.wtns
 end=`date +%s`
 echo "DONE ($((end-start))s)"
 
